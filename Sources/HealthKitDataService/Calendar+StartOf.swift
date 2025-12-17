e0009bedf4e6479f0e6d8241af93501a80492d9a
temamarvel <tema.denisoff@gmail.com>
2025-12-17 11:18:06 +0400

    make extension public

--- Sources/HealthKitDataService/Calendar+StartOf.swift
+++ Sources/HealthKitDataService/Calendar+StartOf.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Calendar {
-    func startOfMonth(for date: Date) -> Date {
+    public func startOfMonth(for date: Date) -> Date {
         let components = dateComponents([.year, .month], from: date)
         return self.date(from: components)!
     }
